3c9fd8cc3f24d1b26c3e003627e23c0eae81742e
bittles <bauern.sc@gmail.com>
2023-01-05 18:53:28 -0500

    use args better

--- bumper/bumper/__init__.py
+++ bumper/bumper/__init__.py
@@ -408,9 +408,9 @@ def main(argv=None):
         )
         parser.add_argument("--debug", action="store_true", help="enable debug logs")
 
-        parser.add_argument("--enable_xmpp", action="store_true", help="enable xmpp server")
+        parser.add_argument("--disable_xmpp", action="store_false", help="enable xmpp server")
 
-        parser.add_argument("--enable_mqtt", action="store_true", help="enable mqtt server")
+        parser.add_argument("--disable_mqtt", action="store_false", help="enable mqtt server")
 
         args = parser.parse_args(args=argv)
 
@@ -423,11 +423,11 @@ def main(argv=None):
         if args.announce:
             bumper_announce_ip = args.announce
 
-        if args.enable_xmpp:
-            bumper_xmpp = True
+        if args.disable_xmpp:
+            bumper_xmpp = False
 
-        if args.enable_mqtt:
-            bumper_mqtt = True
+        if args.disable_mqtt:
+            bumper_mqtt = False
 
         asyncio.run(start())
 
--- bumper/run.sh
+++ bumper/run.sh
@@ -6,7 +6,6 @@ BUMPER_ANNOUNCE_IP=$(bashio::config 'announce_ip')
 TZ=$(bashio::config 'time_zone')
 BUMPER_LISTEN=$(bashio::config 'announce_ip')
 BUMPER_DEBUG=$(bashio::config 'debug_logs')
-ENABLE_XMPP=$(bashio::config 'enable_xmpp_server')
-ENABLE_MQTT=$(bashio::config 'enable_mqtt_server')
-
-python3 -m bumper --listen $BUMPER_LISTEN --announce $BUMPER_ANNOUNCE_IP --debug $BUMPER_DEBUG --enable_xmpp $ENABLE_XMPP --enable_mqtt $ENABLE_MQTT
+ENABLE_XMPP=trueENABLE_MQTT=true
+argline="--listen ${BUMPER_LISTEN} --announce ${BUMPER_ANNOUNCE_IP}"if [ "${BUMPER_DEBUG}" = true ]then    argline=${argline}" --debug"fiif [ "$(bashio::config 'disable_xmpp_server')" = true ]then    argline=${argline}" --disable-xmpp"	ENABLE_XMPP=falsefiif [ "$(bashio::config 'disable_mqtt_server')" = true ]then    argline=${argline}" --disable-mqtt"	ENABLE_MQTT=falsefiecho "Announce IP is ${BUMPER_ANNOUNCE_IP}"echo "Listen IP is ${BUMPER_LISTEN}"echo "Debug is ${BUMPER_DEBUG}"echo "XMPP Server active is ${ENABLE_XMPP}"echo "MQTT Server active is ${ENABLE_MQTT}"echo "python module arguments are:"echo "${argline}"
+python3 -m bumper ${argline}
