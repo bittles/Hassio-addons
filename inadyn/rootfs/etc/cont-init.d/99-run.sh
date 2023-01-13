bdab4dbfaaec7004d14794b731f9928f471de10c
bittles <bauern.sc@gmail.com>
2023-01-13 12:26:12 -0500

    typo

--- inadyn/rootfs/etc/cont-init.d/99-run.sh
+++ inadyn/rootfs/etc/cont-init.d/99-run.sh
@@ -10,7 +10,7 @@ mustache-cli /data/options.json /templates/inadyn.mustache >/etc/inadyn.conf
 /usr/sbin/inadyn --check-config
 
 ## If cache folder not exit then make
-if [ -f /share/inadyn-cache ]; then
+if [ ! -f /share/inadyn-cache ]; then
     mkdir /share/inadyn-cache
     chmod a+r /share/inadyn-cache
 fi
