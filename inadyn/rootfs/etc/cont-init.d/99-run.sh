f1f3bbdedb197c17d80f5094464a42c6a97f0516
bittles <bauern.sc@gmail.com>
2023-01-13 11:23:01 -0500

    having fun with dockerfile

--- inadyn/rootfs/etc/cont-init.d/99-run.sh
+++ inadyn/rootfs/etc/cont-init.d/99-run.sh
@@ -9,6 +9,12 @@ mustache-cli /data/options.json /templates/inadyn.mustache >/etc/inadyn.conf
 # Check it
 /usr/sbin/inadyn --check-config
 
+## If cache folder not exit then make
+if [ -f /share/inadyn-cache ]; then
+    mkdir /share/inadyn-cache
+    chmod +w /share/inadyn-cache
+fi
+
 ##############
 # Launch App #
 ##############
