c488bae608a535ee6691bde36c6cc4858f24c547
bittles <bauern.sc@gmail.com>
2023-01-18 18:39:31 -0500

    Update 99-run.sh

--- inadyn/rootfs/etc/cont-init.d/99-run.sh
+++ inadyn/rootfs/etc/cont-init.d/99-run.sh
@@ -10,10 +10,10 @@ mustache-cli /data/options.json /templates/inadyn.mustache >/etc/inadyn.conf
 /usr/sbin/inadyn --check-config
 
 ## If cache folder not exit then make
-if [ ! -f /share/inadyn-cache ]; then
-    mkdir /share/inadyn-cache
-    chmod a+r /share/inadyn-cache
-fi
+#if [ ! -f /share/inadyn-cache ]; then
+#    mkdir /share/inadyn-cache
+#    chmod a+r /share/inadyn-cache
+#fi
 rm -rf /root/.cache/inadyn
 ln -s /share/inadyn-cache /root/.cache/inadyn
 #chmod a+rw /root/.cache/inadyn
